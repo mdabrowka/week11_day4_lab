66c34154047863678f082dfe19d86c586152ae2f
Marta <martadabrowka@gmail.com>
2017-12-07 15:21:26 +0000

    passed display inventory list

--- specs/record_store_specs.js
+++ specs/record_store_specs.js
@@ -11,6 +11,7 @@ describe('RecordStore', function() {
 beforeEach(function() {
   recordStore = new RecordStore("Zack's Records", "Edinburgh", [], 0);
   record = new Record('Pink Floyd', 'Dark Side of the Moon', 'rock', 7);
+  record1 = new Record('Pink Floyd', 'Wish You Were Here', 'rock', 9);
 })
 
 
@@ -28,9 +29,15 @@ it('record store inventory starts empty', function() {
 
 it('record store balance starts at 0', function() {
   assert.strictEqual(recordStore.balance, 0);
-})
+}),
 
 it('record store can add record to inventory', function(){
   recordStore.addRecord(record);
   assert.strictEqual(recordStore.inventory.length, 1);
+}),
+
+it('record store can list inventory', function(){
+  recordStore.addRecord(record);
+  recordStore.addRecord(record1);
+  assert.deepStrictEqual(recordStore.listInventory(), ["Artist: Pink Floyd, title: Dark Side of the Moon, genre: rock, price: 7", "Artist: Pink Floyd, title: Wish You Were Here, genre: rock, price: 9" ])
 })
