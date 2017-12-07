978fca8b751b9bfdac075f4189e4a4c26939221f
Marta <martadabrowka@gmail.com>
2017-12-07 15:02:09 +0000

    passed properties to string

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -8,21 +8,24 @@ describe('Record', function() {
 
 beforeEach(function() {
   record = new Record('Pink Floyd', 'Dark Side of the Moon', 'rock', 7);
-})
-
+}),
 
 it('record should have an artist', function() {
   assert.strictEqual(record.artist, "Pink Floyd");
-})
+}),
 
 it('record should have a title', function() {
   assert.strictEqual(record.title, "Dark Side of the Moon");
-})
+}),
 
 it('record should have an genre', function() {
   assert.strictEqual(record.genre, "rock");
-})
+}),
 
 it('record should have a price', function() {
   assert.strictEqual(record.price, 7);
+}),
+
+it('record should return properties as a string', function () {
+  assert.strictEqual(record.propertiesToString(), "Artist: Pink Floyd, title: Dark Side of the Moon, genre: rock, price: 7");
 })
